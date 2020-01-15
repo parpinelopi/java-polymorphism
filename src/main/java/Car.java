bf0476eecfc77fce39ced0be665680305d60cb90
Penelope <pennyblack.p@gmail.com>
2020-01-15 11:32:16 +0100

    Car fields added/constructor created/methods for class added

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,17 @@
 public class Car {
+    private String name;
+    private int wheels;
+    private int cylinders;
+    private int doors;
+    private boolean engine;
+    private String emissions;
+
+    public Car(String name, int wheels, int cylinders, boolean engine) {
+        this.name = name;
+        this.wheels = 4;
+        this.cylinders = cylinders;
+        this.engine = true;
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
 
+        class Car {
+            private String name;
+            private int wheels;
+            private int cylinders;
+            private int doors;
+            private int speed;
+            private boolean engine;
+
+
+            public Car(String name, int wheels, int cylinders, int speed, boolean engine) {
+                this.name = name;
+                this.wheels = 4;
+                this.cylinders = cylinders;
+                this.engine = true;
+            }
+
+            public void startEngine(boolean engine) {
+
+                if (engine == true) {
+                    System.out.println("Start engine");
+                } else {
+                    System.out.println("No engine");
+                }
+
+            }
+
+            public void accelerate(int additionalSpeed) {
+                if (speed != 0) {
+                    speed = additionalSpeed + speed;
+                    System.out.println("Accelerate the car by:" + additionalSpeed + "and make total speed" + speed);
+
+                } else {
+                    System.out.println("Start engine first");
+                }
+            }
+        }
     }
 }
